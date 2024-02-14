ccc3a4270f02a790cdb9b1143b9025839d6887ea
limJaemin99 <limjaemin99@gmail.com>
2024-02-14 14:06:49 +0900

    CRUD, Modal

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,11 +1,15 @@
 package org.zerock.b01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
@@ -19,6 +23,7 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    //리스트 출력
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
@@ -28,4 +33,79 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO);
     }
 
+
+    //등록 View
+    @GetMapping("/register")
+    public void registerView(){
+
+    }
+
+
+    //등록 Action
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        log.info("board POST register ..........");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors ..........");
+            // @Valid에서 문제가 발생했을 때, 모든 에러를 'errors'라는 이름으로 전송
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
+
+
+    //특정 번호(bno)의 게시물 조회, 수정 View
+    @GetMapping({"/read", "/modify"})   // {}로 묶어서 여러 경로를 적용할 수 있다.
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info(boardDTO);
+
+        model.addAttribute("dto", boardDTO);
+    }
+
+
+    //수정 Action
+    @PostMapping("/modify")
+    public String modify(PageRequestDTO pageRequestDTO, @Valid BoardDTO boardDTO,
+                         BindingResult bindingResult, RedirectAttributes redirectAttributes){
+        log.info("board modify post ..... " + boardDTO);
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors ..........");
+
+            String link = pageRequestDTO.getLink();
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+            return "redirect:/board/modify?"+link;
+        }
+
+        boardService.modify(boardDTO);
+        redirectAttributes.addFlashAttribute("result", "modified");
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+
+        return "redirect:/board/read";
+    }
+
+
+    //삭제 Action
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes){
+        log.info("remove post ..... " + bno);
+
+        boardService.remove(bno);
+        redirectAttributes.addFlashAttribute("result", "removed");
+
+        return "redirect:/board/list";
+    }
 }
--- src/main/java/org/zerock/b01/dto/BoardDTO.java
+++ src/main/java/org/zerock/b01/dto/BoardDTO.java
@@ -1,5 +1,8 @@
 package org.zerock.b01.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +18,14 @@ public class BoardDTO {
 
     private Long bno;
 
+    @NotEmpty
+    @Size(min = 3, max = 100)
     private String title;
 
+    @NotEmpty
     private String content;
 
+    @NotEmpty
     private String writer;
 
     private LocalDateTime regDate;
